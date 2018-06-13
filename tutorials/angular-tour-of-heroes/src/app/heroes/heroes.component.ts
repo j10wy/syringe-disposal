692a9b612058b68a49649b284456a0c12ee89d60
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-06-13 09:57:52 -0700

    Completed Angular router tutorial

--- tutorials/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tutorials/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,27 +1,22 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Hero } from "../hero";
-import { HeroService } from "../hero.service";
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: "app-heroes",
-  templateUrl: "./heroes.component.html",
-  styleUrls: ["./heroes.component.css"]
+	selector: 'app-heroes',
+	templateUrl: './heroes.component.html',
+	styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
-  constructor(private heroService: HeroService) {}
+	heroes: Hero[];
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    console.log(hero);
-  }
+	constructor(private heroService: HeroService) {}
 
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
-  }
+	ngOnInit() {
+		this.getHeroes();
+	}
 
-  ngOnInit() {
-    this.getHeroes();
-  }
+	getHeroes(): void {
+		this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+	}
 }
