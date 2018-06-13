b62f5802c692f015e74eb7914fceaaba91fb9912
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-06-13 00:03:38 -0700

    [WIP] App routing

--- tutorials/angular-tour-of-heroes/src/app/app.module.ts
+++ tutorials/angular-tour-of-heroes/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
