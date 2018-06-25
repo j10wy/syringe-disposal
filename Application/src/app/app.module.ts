5073349f8a7b9d957264c2914f2910e3e047b14e
Jeff Lowy <jeffreylowy@gmail.com>
2018-06-24 20:11:10 -0700

    Sandbox - save demos/ideas to own component.

--- Application/src/app/app.module.ts
+++ Application/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TrimmerPipe } from './pipes/trimmer.pipe';
 import { LoggerDirective } from './directives/logger.directive';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SandboxComponent } from './components/sandbox/sandbox.component';
 
 @NgModule({
-	declarations: [AppComponent, HomeComponent, TrimmerPipe, LoggerDirective],
+	declarations: [AppComponent, HomeComponent, TrimmerPipe, LoggerDirective, NavbarComponent, SandboxComponent],
 	imports: [AppRoutingModule, BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent]
